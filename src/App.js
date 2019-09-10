e022009632b747b2a11b53622c087cf2ba6ba1d8
Jimmy Jones <jjones@licoa.com>
2019-09-10 15:46:07 -0500

    Added husky and precommit hooks

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+
+const Warning = React.lazy(() => import('./Warning'))
+
+class App extends React.Component {
+  state = {
+    count: 0
+  }
+
+  render() {
+    const { count } = this.state
+    return (
+      <div>
+        <h1>Hello World.</h1>
+        <h2 className={count > 10 ? 'warning' : null}>Count: {count}</h2>
+        <button
+          onClick={() => this.setState(state => ({ count: state.count + 1 }))}
+        >
+          +
+        </button>
+        <button
+          disabled={this.state.count - 1 < 0}
+          onClick={() => this.setState(state => ({ count: state.count - 1 }))}
+        >
+          -
+        </button>
+
+        {count > 10 ? (
+          <React.Suspense fallback={null}>
+            <Warning />
+          </React.Suspense>
+        ) : null}
+      </div>
+    )
+  }
+}
+
+export default hot(module)(App)
